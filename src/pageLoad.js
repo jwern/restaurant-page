f6c5315a4bd035541cf28614490020e62905ca55
Jillian Werner <jillianlwerner@gmail.com>
2021-01-25 17:20:47 -0500

    Add nav links

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,6 +1,8 @@
 function initialPageLoad() {
   let pageContent = document.getElementById('content');
-  
+
+  let pageTabs = createTabs();
+
   let headerImage = document.createElement('img');
   headerImage.src = "images/header_image.jpg";
   headerImage.id = "header-image";
@@ -14,6 +16,7 @@ function initialPageLoad() {
   pageText.innerText = "Welcome!  We have waffles, juice, eggs - you know, breakfast things.";
 
   let children = [
+    pageTabs,
     headerImage, 
     pageTitle, 
     pageText
@@ -22,6 +25,43 @@ function initialPageLoad() {
   for (let child of children) {
     pageContent.appendChild(child);
   };
+}
+
+function createTabs() {
+  let tabsDiv = document.createElement('div');
+  tabsDiv.classList.add('nav-links');
+
+  let navLinks = document.createElement('ul');
+  navLinks.id = "navigation";
+
+  let navHome = createListItem("Home");
+  let navMenu = createListItem("Menu");
+  let navContact = createListItem("Contact");
+
+  let navChildren = [
+    navHome,
+    navMenu,
+    navContact
+  ];
+
+  for (let child of navChildren) {
+    navLinks.appendChild(child);
+  };
+
+  tabsDiv.appendChild(navLinks);
+
+  return tabsDiv;
+}
+
+function createListItem(text) {
+  let listItem = document.createElement('li');
+  let listAnchor = document.createElement('a');
+  
+  listAnchor.href = `#${text}`;
+  listAnchor.innerText = text;
+
+  listItem.appendChild(listAnchor);
+  return listItem;
 }
 
 export { initialPageLoad };
