e17c55005f8c371cbec835e4249e3e7f91643b7c
Jillian Werner <jillianlwerner@gmail.com>
2021-01-25 16:52:49 -0500

    Create initial page load module with basic intro content

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("Testing webpack");
+(()=>{"use strict";!function(){let e=document.getElementById("content"),t=document.createElement("img");t.src="images/header_image.jpg",t.id="header-image";let n=document.createElement("h1");n.id="page-title",n.innerText="Breakfast Bar";let a=document.createElement("p");a.id="page-intro",a.innerText="Welcome!  We have waffles, juice, eggs - you know, breakfast things.";let i=[t,n,a];for(let t of i)e.appendChild(t)}()})();
